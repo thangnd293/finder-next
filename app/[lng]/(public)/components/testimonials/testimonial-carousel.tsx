ede12eb093fb70f76622ebd4f364b4b05f7210d5
Nguyen Dac Thang [thangnd] <thangnd@nexondv.com>
2023-07-25 17:40:38 +0700

    feat: add ui testimonial section

--- app/[lng]/(public)/components/testimonials/testimonial-carousel.tsx
+++ app/[lng]/(public)/components/testimonials/testimonial-carousel.tsx
@@ -3,63 +3,76 @@
 import ActionIcon from "@/components/action-icon";
 import { ArrowLeftIcon, ArrowRightIcon } from "@radix-ui/react-icons";
 import React from "react";
-import Carousel from "react-multi-carousel";
-import Testimonial from "./testimonial";
-import "react-multi-carousel/lib/styles.css";
 
-const responsive = {
-  superLargeDesktop: {
-    breakpoint: { max: 4000, min: 3000 },
-    items: 5,
-  },
-  desktop: {
-    breakpoint: { max: 3000, min: 870 },
-    items: 3,
-  },
-  tablet: {
-    breakpoint: { max: 870, min: 600 },
-    items: 2,
-  },
-  mobile: {
-    breakpoint: { max: 600, min: 0 },
-    items: 1,
-  },
+import Slider from "react-slick";
+
+import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
+import Testimonial from "./testimonial";
+
+const settings = {
+  arrows: false,
+  dots: false,
+  swipeToSlide: true,
+  infinite: true,
+  slidesToShow: 3,
+  autoplay: true,
+  centerMode: true,
+  pauseOnHover: true,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+      },
+    },
+    {
+      breakpoint: 870,
+      settings: {
+        slidesToShow: 2,
+      },
+    },
+    {
+      breakpoint: 500,
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+  ],
 };
 
 export default function TestimonialCarousel() {
+  const sliderRef = React.useRef<Slider>(null);
+
+  const nextSlide = () => {
+    sliderRef.current?.slickNext();
+  };
+
+  const previousSlide = () => {
+    sliderRef.current?.slickPrev();
+  };
+
   return (
     <div className="my-own-custom-container">
-      <Carousel
-        className="mr-2 translate-x-2"
-        itemClass="pr-4"
-        responsive={responsive}
-        ssr
-        infinite
-        autoPlay
-        keyBoardControl
-        arrows={false}
-        renderButtonGroupOutside
-        customButtonGroup={<ButtonGroup />}
-      >
-        {Array(10)
-          .fill(0)
-          .map((_, i) => (
-            <Testimonial key={i} />
-          ))}
-      </Carousel>
+      <div>
+        <Slider className="[&_.slick-slide]:px-2" ref={sliderRef} {...settings}>
+          {Array(10)
+            .fill(0)
+            .map((_, i) => (
+              <Testimonial key={i} />
+            ))}
+        </Slider>
+      </div>
+
+      <div className="carousel-button-group mx-auto mt-4 w-fit space-x-4">
+        <ActionIcon className="h-12 w-12" onClick={previousSlide}>
+          <ArrowLeftIcon />
+        </ActionIcon>
+        <ActionIcon className="h-12 w-12" onClick={nextSlide}>
+          <ArrowRightIcon />
+        </ActionIcon>
+      </div>
     </div>
   );
 }
-
-const ButtonGroup = ({ next, previous }: any) => {
-  return (
-    <div className="carousel-button-group mx-auto mt-3 w-fit space-x-4">
-      <ActionIcon className="h-12 w-12" onClick={previous}>
-        <ArrowLeftIcon />
-      </ActionIcon>
-      <ActionIcon className="h-12 w-12" onClick={next}>
-        <ArrowRightIcon />
-      </ActionIcon>
-    </div>
-  );
-};
--- app/[lng]/(public)/components/testimonials/testimonial.tsx
+++ app/[lng]/(public)/components/testimonials/testimonial.tsx
@@ -1,15 +1,15 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
-import { libreBaskerville } from "@/fonts";
+import { libreBaskerville } from "@/assets/fonts";
 import { cn } from "@/lib/utils";
 import React from "react";
 
 export default function Testimonial() {
   return (
-    <Card className="bg-background max-w-md">
+    <Card className="bg-background">
       <CardHeader>
         <CardTitle
-          className={cn(libreBaskerville.className, "text-3xl relative mb-1")}
+          className={cn(libreBaskerville.className, "relative mb-1 text-3xl")}
         >
           Hạnh
           <span className="absolute right-0 text-9xl text-gray-300">“</span>
@@ -17,7 +17,7 @@ export default function Testimonial() {
         <Separator className="t-1" />
       </CardHeader>
       <CardContent>
-        <p className="font-light min-h-[200px]">
+        <p className="min-h-[200px] font-light">
           Sau vài tháng hẹn hò, mối quan hệ của chúng tôi đứng trước một ngã rẽ
           và chúng tôi quyết định dừng lại. Tôi đã yêu người ấy nhưng đã nghĩ là
           chuyện sẽ chẳng đi đến đâu. Vậy mà 9 tháng sau chúng tôi chẳng thể xa
