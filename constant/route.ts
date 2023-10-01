d032c5f60c84ed97ccd6921821d8c6f9a5a935f2
thangnd293 <thangnd293@gmail.com>
2023-10-01 14:34:29 +0700

    feat: setting page

---
+++ constant/route.ts
@@ -0,0 +1,8 @@
+export const ROUTE = {
+  HOME: "/app",
+  PROFILE: "/app/profile",
+  SETTING: "/app/settings",
+  EDIT_PROFILE: "/app/profile/edit",
+  EXPLORE: "/app/explore",
+  DATING: "/app/dating",
+};
