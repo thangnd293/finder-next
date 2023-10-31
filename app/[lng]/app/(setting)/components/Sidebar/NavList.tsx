5e2cea0d390b944680bbfb3ff836b8833fe4f0e1
phatphamm01 <phatphamm01@gmail.com>
2023-11-01 03:12:06 +0700

    feat: face verify

--- app/[lng]/app/(setting)/components/Sidebar/NavList.tsx
+++ app/[lng]/app/(setting)/components/Sidebar/NavList.tsx
@@ -14,6 +14,7 @@ import {
   BsFillPersonFill,
 } from "react-icons/bs";
 import { useMediaQuery } from "usehooks-ts";
+import Verified, { verifyAction } from "../../verified/verified";
 
 const NavList = () => {
   const pathname = usePathname();
@@ -21,30 +22,33 @@ const NavList = () => {
   const isMobileView = useMediaQuery("(max-width: 768px)");
 
   return (
-    <nav className="space-y-3 p-4">
-      {navList.map(({ Icon, label, href, action }) => {
-        const Component = href ? Link : "button";
+    <>
+      <nav className="space-y-3 p-4">
+        {navList.map(({ Icon, label, href, action }) => {
+          const Component = href ? Link : "button";
 
-        const props = href ? { href } : { onClick: action };
+          const props = href ? { href } : { onClick: action };
 
-        return (
-          <Tooltip label={label} key={label} disabled={!isMobileView}>
-            <Component
-              className={cn(
-                "flex aspect-square w-full items-center justify-center rounded-full p-1.5 text-center font-medium text-secondary-foreground hover:bg-background-50 hover:text-foreground md:block md:aspect-auto md:p-2.5",
-                {
-                  "text-foreground": href && pathname?.includes(href),
-                },
-              )}
-              {...(props as any)}
-            >
-              <Icon className="block h-5 w-5 md:hidden" />
-              <span className="hidden md:block">{label}</span>
-            </Component>
-          </Tooltip>
-        );
-      })}
-    </nav>
+          return (
+            <Tooltip label={label} key={label} disabled={!isMobileView}>
+              <Component
+                className={cn(
+                  "flex aspect-square w-full items-center justify-center rounded-full p-1.5 text-center font-medium text-secondary-foreground hover:bg-background-50 hover:text-foreground md:block md:aspect-auto md:p-2.5",
+                  {
+                    "text-foreground": href && pathname?.includes(href),
+                  },
+                )}
+                {...(props as any)}
+              >
+                <Icon className="block h-5 w-5 md:hidden" />
+                <span className="hidden md:block">{label}</span>
+              </Component>
+            </Tooltip>
+          );
+        })}
+      </nav>
+      <Verified />
+    </>
   );
 };
 
@@ -60,6 +64,13 @@ const navList: NavItem[] = [
     href: ROUTE.SETTING,
     label: "Cài đặt",
     Icon: BsFillGearFill,
+  },
+  {
+    action: () => {
+      verifyAction.setOpen(true);
+    },
+    label: "Xác thực tài khoản",
+    Icon: BsFillPersonFill,
   },
   {
     action: () => {
